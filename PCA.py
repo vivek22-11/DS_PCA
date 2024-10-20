db4115e7cf403dccd3a5a8fdf39e4c9ef25f4fd4
VIVEK KADAM <vivekkadam086@gmail.com>
2024-10-20 17:19:08 +0530

    first commit

---
+++ PCA.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 16:24:02 2023
+
+@author: Vivek
+"""
+
+import pandas as pd
+import numpy as np
+uni1=pd.read_excel("C:/Data Set/University_Clustering.xlsx")
+uni1.describe()
+
+uni1.info()
+uni=uni1.drop(["State"],axis=1) 
+uni
+
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import scale
+
+#considering only numerical data
+uni.data=uni.iloc[:, 1:]
+
+#normalizing the numerical data
+uni_normal = scale(uni.data)
+uni_normal
+
+pca=PCA(n_components =6)
+pca_values=pca.fit_transform(uni_normal)
+
+var = pca.explained_variance_ratio_
+var
+
+#PCA weights
+#pca.components_
+#pca.components__[0]
+
+#cumulative variance
+var1 = np.cumsum(np.round(var, decimals=4)*100)
+var1
+
+#variance plot for PCA components obtained
+plt.plot(var1, color= "red")
+
+#PCA scores
+pca_values
+
+
+pca_data =pd.DataFrame(pca_values)
+pca_data.columns="comp0","comp1","comp2","comp3","comp4","comp5"
+final=pd.cocat([uni.Univ ,pca_data.iloc[:, 0:3]], axis=1)
+
+#this is Univ  column of uni data frame
+
+#scatter diagram
+
+import matplotlib.pylab as plt
+ax =final.plot(x="comp0",y="comp1",kind="scatter",figsize=(12,8))
+final[['comp0','comp1','Univ']].apply(lambda x: ax.text(*x), axis=1)
